8b30ce1b9ab091138283a7ff1309c2714a6a82ff
AurelienLeandri <aurelien.leandri@gmail.com>
2016-10-13 16:10:47 +0200

    Add constructor for class connection

--- UnityPlayer/UI/Assets/Scripts/DZConnection.cs
+++ UnityPlayer/UI/Assets/Scripts/DZConnection.cs
@@ -41,15 +41,47 @@ enum dz_connect_event_t {
 	DZ_CONNECT_EVENT_USER_NEW_OPTIONS,
 	DZ_CONNECT_EVENT_ADVERTISEMENT_START,
 	DZ_CONNECT_EVENT_ADVERTISEMENT_STOP
-}
+};
 
 delegate void dz_activity_operation_callback(IntPtr d, IntPtr data, int status, IntPtr result);
 delegate void dz_connect_onevent_cb(IntPtr connectHandle, IntPtr eventHandle, IntPtr data);
 delegate bool dz_connect_crash_reporting_delegate();
 
-public static class DZConnection {
+[Serializable()]
+public class ConnectionInitFailedException : System.Exception
+{
+	public ConnectionInitFailedException() : base() { }
+	public ConnectionInitFailedException(string message) : base(message) { }
+	public ConnectionInitFailedException(string message, System.Exception inner) : base(message, inner) { }
+	protected ConnectionInitFailedException(System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context) { }
+}
 
-	// COMMON
+[Serializable()]
+public class RequestFailedException : System.Exception
+{
+	public RequestFailedException() : base() { }
+	public RequestFailedException(string message) : base(message) { }
+	public RequestFailedException(string message, System.Exception inner) : base(message, inner) { }
+	protected RequestFailedException(System.Runtime.Serialization.SerializationInfo info,
+		System.Runtime.Serialization.StreamingContext context) { }
+}
+
+public class DZConnection {
+	public DZConnection(dz_connect_configuration config, IntPtr context) {
+		handle = dz_connect_new (ref config);
+		if (!handle)
+			throw new ConnectionInitFailedException ("Connection handle failed to initialize. Check connection info you gave");
+		if (dz_connect_activate handle, context))
+			throw new RequestFailedException ("Connection failed to activate.");
+	}
+
+	public int GetDeviceId() {
+		return dz_connect_get_device_id (handle);
+	}
+
+	private IntPtr handle;
+
 	[DllImport("libdeezer")] public static extern void dz_object_release(IntPtr objectHandle);
 	[DllImport("libdeezer")] public static extern IntPtr dz_connect_new(ref dz_connect_configuration config);
 	[DllImport("libdeezer")] public static extern IntPtr dz_connect_get_device_id(IntPtr self);
@@ -59,9 +91,4 @@ public static class DZConnection {
 	[DllImport("libdeezer")] public static extern int dz_connect_offline_mode(IntPtr self, dz_activity_operation_callback cb, IntPtr data, bool offline_mode_forced);
 	[DllImport("libdeezer")] public static extern int dz_connect_event_get_type(IntPtr eventHandle);
 	[DllImport("libdeezer")] public static extern int dz_connect_deactivate(IntPtr connectHandle, dz_activity_operation_callback cb, IntPtr data);
-
-	// CONNECT
-
-
-
 }
