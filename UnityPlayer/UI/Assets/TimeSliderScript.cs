07abf93e062913016b4097d6f592d7a1f6dbe872
AurelienLeandri <aurelien.leandri@gmail.com>
2016-10-27 11:32:34 +0200

    Add base script for time slider

---
+++ UnityPlayer/UI/Assets/TimeSliderScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeSliderScript : MonoBehaviour {
+	public Slider SliderComponent { get; private set; }
+	private MyDeezerApp app;
+
+	// Use this for initialization
+	void Awake () {
+		SliderComponent = transform.GetComponent<Slider> ();
+		app = GameObject.Find ("AppObject").GetComponent<DeezerAppLaunchScript> ().app;
+	}
+
+	public void OnValueChanged(int value) {
+		app.Seek (value);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
