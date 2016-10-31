a3eed0a67e8dd575c17836874841b9d00ecb98b2
AurelienLeandri <aurelien.leandri@gmail.com>
2016-10-31 10:33:47 +0100

    Add intermediate child class to MonoBehaviour with a reference to ApplicationMainScript

---
+++ UnityPlayer/UI/Assets/Scripts/ApplicationElement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class ApplicationElement : MonoBehaviour
+{
+	public ApplicationMainScript MainView {
+		get { return GameObject.Find ("AppObject").GetComponent<ApplicationMainScript> (); }
+		private set { MainView = value; }
+	}
+}
+
